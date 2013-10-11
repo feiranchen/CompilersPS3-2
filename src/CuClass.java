5c3364ff6f0540a92f35313cccc8767ef50e63b2
yinglei <yw437@cornell.edu>
2013-10-11 04:23:34 -0400

    merge

--- src/CuClass.java
+++ src/CuClass.java
@@ -5,23 +5,19 @@ import java.util.Map;
 import java.util.LinkedHashMap;
 import java.util.Map.Entry;
 
-//Yinglei worked on the desugared program (core language), in parsing, we should
-//put any implementation in interface to class, and this is done in parsing, probably haven't done it yet
 public abstract class CuClass {
 	protected String text = "";
 	String name;
-	CuType            superType = new Top();
-	List<CuExpr>        superArg;
-	List<CuStat> classStatement = new ArrayList<CuStat>();
-	HashMap<String, CuFun>  funList = new HashMap<String, CuFun>();
-	List<String>       kindPara = null;
-	Map<String, CuType> fieldTypes=new LinkedHashMap<String,CuType>();
+	CuType            superType = new Top();	
+	List<String>       kindCtxt = null;
+	Map<String,CuTypeScheme> mFunctions = new HashMap<String,CuTypeScheme>();
 	
-	List<CuType> appliedTypePara=new ArrayList<CuType>();
+	HashMap<String, CuFun>  funList = new HashMap<String, CuFun>();
+
 	public void add(List<CuExpr> s) {}
 	public void addSuper(CuType t) {}
 	public void add(CuStat s) {}
-	public void addFun(String v, CuTypeScheme ts, CuStat s) {}
+	public void add(String v, CuTypeScheme ts, CuStat s) {}
 	public void add(String v_name, CuTypeScheme ts) {}
 	public void add(CuVvc v_name, CuTypeScheme ts) {}
 	public boolean isInterface() {return false; }
@@ -33,10 +29,18 @@ public abstract class CuClass {
 }
 
 class Cls extends CuClass {
+	Map<String, CuType> fieldTypes=new LinkedHashMap<String,CuType>();
+	
+	List<CuType> appliedTypePara=new ArrayList<CuType>();
+	List<CuStat> classStatement = new ArrayList<CuStat>();
+	//private static final Exception NoSuchTypeExpression() = null;
+	
+	List<CuExpr>        superArg;
+
 	public Cls(String clsintf, List<String> kc, LinkedHashMap<String, CuType> tc) {
-		name=clsintf;
-		kindPara=kc;
-		fieldTypes=tc;
+		super.name=clsintf;
+		super.kindCtxt=kc;
+		this.fieldTypes=tc;
 	}
 
 	@Override public void add (CuStat s) {
@@ -44,17 +48,24 @@ class Cls extends CuClass {
 
 	//TODO: grab all the methods here
 	@Override public void addSuper (CuType tt) {
-		superType = tt;}
+		super.superType = tt;}
 	
-	@Override public void addFun(String v, CuTypeScheme ts, CuStat s) {
-		funList.put(v,new Function(v,ts,s));
+	@Override public void add(String v, CuTypeScheme ts, CuStat s) {
+		super.funList.put(v,new Function(v,ts,s));
+		super.mFunctions.put(v, ts);
 	}
 	
 	@Override public CuClass calculateType(CuContext context) throws NoSuchTypeException { 
 		context.updateClass(name, this);
 		if (superType instanceof VClass){
 			Map<String, CuFun> superfunLst= context.mClasses.get(superType.id).funList;
-			context.updateType(name, new VClass(name, new ArrayList<CuType>()));
+			//also need to update the function context
+			List<CuType> cur_types = new ArrayList<CuType>();
+			for (String str : super.kindCtxt) {
+				cur_types.add(new VTypePara(str));
+			}
+			CuTypeScheme temp_ts = new TypeScheme(super.kindCtxt, this.fieldTypes , new VClass(super.name, cur_types));
+			context.updateFunction(super.name, temp_ts);
 			for (CuStat s :classStatement) {s.calculateType(context);}
 			for (Entry<String, CuType> e: fieldTypes.entrySet()) {e.getValue().calculateType(context);}
 			
@@ -74,6 +85,8 @@ class Cls extends CuClass {
 				if (!s.calculateType(context).b
 						||s.calculateType(context).tau.isSubtypeOf(s.calculateType(context).tau)) 
 					throw new NoSuchTypeException();}
+			
+			
 		}
 		else if(superType instanceof VTypeInter) {
 			for (CuType t:superType.parentType){
@@ -107,12 +120,16 @@ class Cls extends CuClass {
 }
 
 class Intf extends CuClass{
+	Map<String, CuType> fieldTypes=new LinkedHashMap<String,CuType>();
+	
+	List<CuType> appliedTypePara=new ArrayList<CuType>();
+	List<CuStat> classStatement = new ArrayList<CuStat>();
 	
 	public Intf (String iname, List<String> kname){
 		
 		//for printing
 		text = "interface " + name.toString() + " <";
-		for (String s : kindPara) {
+		for (String s : kindCtxt) {
 			text += " " + s.toString();
 		}
 		text += " > extends";
@@ -121,8 +138,9 @@ class Intf extends CuClass{
 	public void addSuper (CuType tt) throws NoSuchTypeException{
 		superType = tt;
 	}
-	@Override public void addFun(String v, CuTypeScheme ts, CuStat s) {
+	@Override public void add(String v, CuTypeScheme ts, CuStat s) {
 		funList.put(v,new Function(v,ts,s));
+		super.mFunctions.put(v, ts);
 	}
 	
 	@Override public CuClass calculateType(CuContext cTxt) throws NoSuchTypeException {
@@ -172,151 +190,73 @@ class Intf extends CuClass{
 }
 
 
-
-
 //======Class init=========
 
 class VBoolean extends Cls {
 	Boolean v=false;
 	public VBoolean() {
 		super("Boolean", new ArrayList<String>(), new LinkedHashMap<String, CuType>());
-		funList = new HashMap<String, CuFun>();
-		funList.put("negate", new Function("negate",
-				new TypeScheme(new ArrayList<String>(), new LinkedHashMap<String,CuType>(), CuType.bool)));
-		funList.put("and", new Function("and",
-				new TypeScheme(new ArrayList<String>(), 
-						new LinkedHashMap<String,CuType>(){{put("that",CuType.bool);}}, 
-						CuType.bool)));
-		funList.put("or", new Function("or",
-				new TypeScheme(new ArrayList<String>(), 
-						new LinkedHashMap<String,CuType>(){{put("that",CuType.bool);}}, 
-						CuType.bool)));
-		funList.put("through", new Function("through",
-				new TypeScheme(new ArrayList<String>(), 
-						new LinkedHashMap<String,CuType>()
-						{{put("upper",CuType.bool);
-						  put("includeLower",CuType.bool);
-						  put("includeUpper",CuType.bool);}}, 
-						CuType.iterable(CuType.bool))));
-		funList.put("onwards", new Function("onwards",
-				new TypeScheme(new ArrayList<String>(),
-						new LinkedHashMap<String,CuType>(){{put("inclusive",CuType.bool);}}, 
-						CuType.iterable(CuType.bool))));
-		funList.put("lessThan", new Function("lessThan",
-				new TypeScheme(new ArrayList<String>(),
-						new LinkedHashMap<String,CuType>(){{
-							put("that",CuType.bool);
-							put("strict",CuType.bool);}}, 
-						CuType.bool)));
-		funList.put("equals", new Function("equals",
-				new TypeScheme(new ArrayList<String>(),
-						new LinkedHashMap<String,CuType>(){{put("that",CuType.bool);}}, 
-						CuType.bool)));
-		
 		//if (val instanceof Boolean) { v=val; }
 		//else { throw new NoSuchTypeException();}
+		//Just one example, we also need to add other methods
+		CuTypeScheme ts;
+		ts = new TypeScheme(new ArrayList<String>(), new HashMap<String, CuType>(), CuType.bool);
+		super.mFunctions.put("negate", ts);
 	}
-	public VBoolean calculateType(CuContext ctxt) { 
-		ctxt.updateClass(name, this);
-		return this; }
+    
+	public boolean calculateType() { return v; }
 }
 
 class VInteger extends Cls {
 	Integer v=0;
 	public VInteger() {
 		super("Integer", new ArrayList<String>(), new LinkedHashMap<String, CuType>());
-		funList = new HashMap<String, CuFun>();
-
-		public Function (String v_input, CuTypeScheme ts_input){
-		funList.put("negative", new Function("negative",
-				new TypeScheme(new ArrayList<String>(),
-						new LinkedHashMap<String,CuType>(),
-						CuType.integer)));
-		funList.put("times", new Function("times",
-				new TypeScheme(new ArrayList<String>(), 
-						new LinkedHashMap<String,CuType>(){{put("factor",CuType.integer);}}, 
-						CuType.iterable(CuType.integer))));
-		funList.put("divide", new Function("divide",
-				new TypeScheme(new ArrayList<String>(), 
-						new LinkedHashMap<String,CuType>(){{put("divisor",CuType.integer);}},  
-						CuType.iterable(CuType.integer))));
-		funList.put("modulo", new Function("modulo",
-				new TypeScheme(new ArrayList<String>(), 
-						new LinkedHashMap<String,CuType>(){{put("modulus",CuType.integer);}},  
-						CuType.iterable(CuType.integer))));
-		funList.put("plus", new Function("plus",
-				new TypeScheme(new ArrayList<String>(), 
-						new LinkedHashMap<String,CuType>(){{put("summand",CuType.integer);}}, 
-						CuType.integer)));
-		funList.put("minus", new Function("minus",
-				new TypeScheme(new ArrayList<String>(), 
-						new LinkedHashMap<String,CuType>(){{put("subtrahend",CuType.integer);}}, 
-						CuType.integer)));
-		funList.put("through", new Function("through",
-				new TypeScheme(new ArrayList<String>(), 
-						new LinkedHashMap<String,CuType>()
-						{{put("upper",CuType.integer);
-						  put("includeLower",CuType.bool);
-						  put("includeUpper",CuType.bool);}},  
-						CuType.iterable(CuType.integer))));
-		funList.put("onwards", new Function("onwards",
-				new TypeScheme(new ArrayList<String>(), 
-						new LinkedHashMap<String,CuType>(){{put("inclusive",CuType.bool);}}, 
-						CuType.iterable(CuType.integer))));
-		funList.put("lessThan", new Function("lessThan",
-				new TypeScheme(new ArrayList<String>(), 
-						new LinkedHashMap<String,CuType>()
-						{{put("that",CuType.integer);
-						  put("strict",CuType.bool);}}, 
-						CuType.bool)));
-		funList.put("equals", new Function("equals",
-				new TypeScheme(new ArrayList<String>(), 
-						new LinkedHashMap<String,CuType>(){{put("that",CuType.integer);}}, 
-						CuType.bool)));
 		//if (val instanceof Integer) { v=val; }
 		//else { throw new NoSuchTypeException();}
+		CuTypeScheme ts;
+		ts = new TypeScheme(new ArrayList<String>(), new HashMap<String, CuType>(), CuType.integer);
+		super.mFunctions.put("negative", ts);
+		
+		HashMap<String, CuType> arg = new HashMap<String, CuType>();
+		arg.put("operator", CuType.integer);
+		ts = new TypeScheme(new ArrayList<String>(), arg, CuType.integer);
+		super.mFunctions.put("times", ts);
+		super.mFunctions.put("plus", ts);
+		super.mFunctions.put("minus", ts);
+		super.mFunctions.put("equals", ts);
+		ts = new TypeScheme(new ArrayList<String>(), arg, new Iter(CuType.integer));
+		super.mFunctions.put("divide", ts);
+		super.mFunctions.put("modulo", ts);
+		
+		arg = new HashMap<String, CuType>();
+		arg.put("upper", CuType.integer);
+		arg.put("includeLower", CuType.bool);
+		arg.put("includeUpper", CuType.bool);
+		ts = new TypeScheme(new ArrayList<String>(), arg, new Iter(CuType.integer));
+		super.mFunctions.put("through", ts);
 	}
-	public VInteger calculateType(CuContext ctxt) { 
-		ctxt.updateClass(name, this);
-		return this; }
+	public VInteger calculateType() { return this; }
 }
 
 class VCharacter extends Cls {
 	Character c;
 	public VCharacter() {
 		super("Character", new ArrayList<String>(), new LinkedHashMap<String, CuType>());
-		funList.put("unicode", new Function("unicode",
-				new TypeScheme(new ArrayList<String>(), 
-						new LinkedHashMap<String,CuType>(), 
-						CuType.integer)));
-		funList.put("equals", new Function("equals",
-				new TypeScheme(new ArrayList<String>(), 
-						new LinkedHashMap<String,CuType>(){{put("that",CuType.character);}}, 
-						CuType.bool)));
 		//if (val instanceof Character) { c=val; }
 		//else { throw new NoSuchTypeException();}
 	}
-	public VCharacter calculateType(CuContext ctxt) { return this; }
+	public VCharacter calculateType() { return this; }
 }
 
 class VString extends Cls {
 	String v="";
 	public VString() {
 		super("String", new ArrayList<String>(), new LinkedHashMap<String, CuType>());
-		funList = new HashMap<String, CuFun>();
-		funList.put("plus", new Function("plus",
-				new TypeScheme(new ArrayList<String>(), 
-						new LinkedHashMap<String,CuType>(){{put("summand",CuType.integer);}}, 
-						CuType.integer)));
-		funList.put("minus", new Function("minus",
-				new TypeScheme(new ArrayList<String>(), 
-						new LinkedHashMap<String,CuType>(){{put("subtrahend",CuType.integer);}}, 
-						CuType.integer)));
 		//if (val instanceof String) { v=val; }
 		//else { throw new NoSuchTypeException();}
 	}
 
-	public VString calculateType(CuContext ctxt) { return this; }
+	public VString calculateType() { return this; }
 }
 
 
@@ -324,15 +264,10 @@ class VIterable extends Cls {
 	List<CuType> v;
 	public VIterable(List<String> kc) {
 		super("Iterable", kc, new LinkedHashMap<String, CuType>());
-		funList = new HashMap<String, CuFun>();
-		funList.put("equals", new Function("equals",
-				new TypeScheme(new ArrayList<String>(), 
-						new LinkedHashMap<String,CuType>(){{put("that",CuType.string);}}, 
-						CuType.bool)));
 		//if (val instanceof List<CuType>) { v=val; }
 		//else { throw new NoSuchTypeException();}
+		this.addSuper(new Iter(CuType.character));
 	}
-	
-	public VIterable calculateType(CuContext ctxt) { return this; }
+	public VIterable calculateType() { return this; }
 }
 
